0670a22abbb196c69ed580ee65026e099a629e19
Pedro J <pjjimenezochoa@gmail.com>
2026-09-02 20:55:29 -0500

    fix(restaurant): extend specific DomainException subclasses instead of generic DomainException

--- src/contexts/restaurant/floor-element/domain/exceptions/floor-element-not-exist.exception.ts
+++ src/contexts/restaurant/floor-element/domain/exceptions/floor-element-not-exist.exception.ts
@@ -1,6 +1,6 @@
-import { DomainException } from '@shared/domain/exceptions/domain.exception'
+import { NotFoundException } from '@shared/domain/exceptions/domain.exception'
 
-export class FloorElementNotExist extends DomainException {
+export class FloorElementNotExist extends NotFoundException {
   constructor(id: string) {
     super(`Floor element with id ${id} does not exist`)
   }
--- src/contexts/restaurant/zone/domain/exceptions/zone-name-already-exists.exception.ts
+++ src/contexts/restaurant/zone/domain/exceptions/zone-name-already-exists.exception.ts
@@ -1,6 +1,6 @@
-import { DomainException } from '@shared/domain/exceptions/domain.exception'
+import { BusinessRuleViolationException } from '@shared/domain/exceptions/domain.exception'
 
-export class ZoneNameAlreadyExists extends DomainException {
+export class ZoneNameAlreadyExists extends BusinessRuleViolationException {
   constructor(name: string) {
     super(`Ya existe una zona con el nombre "${name}"`)
   }
