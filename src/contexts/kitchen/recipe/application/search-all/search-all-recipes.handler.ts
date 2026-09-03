710fae931698745ea7627f219fa8ae394325757b
Pedro J <pjjimenezochoa@gmail.com>
2026-09-02 19:35:16 -0500

    fix(kitchen/recipe): return RecipeResponse from query handlers, not raw aggregate
    FindRecipeHandler and SearchAllRecipesHandler returned the domain
    Recipe aggregate directly, forcing recipe.controller.ts to import a
    Domain type just to type-annotate the query bus result before mapping
    it to a response DTO.
    
    Handlers now map to RecipeResponse.fromDomain() before returning, per
    onion-architecture Rule 5 (response DTOs are the use case's output
    contract, not the controller's job) and onion-presentation Rule 2 (a
    controller needing a Domain import signals the DTO boundary is
    misplaced). recipe.response.ts moves to application/dto/ to match.

--- src/contexts/kitchen/recipe/application/search-all/search-all-recipes.handler.ts
+++ src/contexts/kitchen/recipe/application/search-all/search-all-recipes.handler.ts
@@ -1,13 +1,14 @@
 import { QueryHandler, IQueryHandler } from '@nestjs/cqrs'
 import { SearchAllRecipesQuery } from './search-all-recipes.query'
 import { SearchAllRecipes } from './search-all-recipes'
-import { Recipe } from '@contexts/kitchen/recipe/domain/recipe'
+import { RecipeResponse } from '@contexts/kitchen/recipe/application/dto/recipe.response'
 
 @QueryHandler(SearchAllRecipesQuery)
 export class SearchAllRecipesHandler implements IQueryHandler<SearchAllRecipesQuery> {
   constructor(private readonly useCase: SearchAllRecipes) {}
 
-  async execute(_query: SearchAllRecipesQuery): Promise<Recipe[]> {
-    return this.useCase.run()
+  async execute(_query: SearchAllRecipesQuery): Promise<RecipeResponse[]> {
+    const recipes = await this.useCase.run()
+    return recipes.map(recipe => RecipeResponse.fromDomain(recipe))
   }
 }
--- src/contexts/kitchen/recipe/presentation/http/controllers/recipe.controller.ts
+++ src/contexts/kitchen/recipe/presentation/http/controllers/recipe.controller.ts
@@ -12,13 +12,12 @@ import {
 import { CommandBus, QueryBus } from '@nestjs/cqrs'
 import { CreateRecipeRequest } from '../dto/create-recipe.request'
 import { UpdateRecipeRequest } from '../dto/update-recipe.request'
-import { RecipeResponse } from '../dto/recipe.response'
+import { RecipeResponse } from '@contexts/kitchen/recipe/application/dto/recipe.response'
 import { CreateRecipeCommand } from '@contexts/kitchen/recipe/application/create/create-recipe.command'
 import { FindRecipeQuery } from '@contexts/kitchen/recipe/application/find/find-recipe.query'
 import { SearchAllRecipesQuery } from '@contexts/kitchen/recipe/application/search-all/search-all-recipes.query'
 import { UpdateRecipeCommand } from '@contexts/kitchen/recipe/application/update/update-recipe.command'
 import { DeleteRecipeCommand } from '@contexts/kitchen/recipe/application/delete/delete-recipe.command'
-import { Recipe } from '@contexts/kitchen/recipe/domain/recipe'
 
 @Controller('recipes')
 export class RecipeController {
@@ -44,15 +43,13 @@ export class RecipeController {
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<RecipeResponse> {
     const query = new FindRecipeQuery(id)
-    const recipe: Recipe = await this.queryBus.execute(query)
-    return RecipeResponse.fromDomain(recipe)
+    return this.queryBus.execute(query)
   }
 
   @Get()
   async findAll(): Promise<RecipeResponse[]> {
     const query = new SearchAllRecipesQuery()
-    const recipes: Recipe[] = await this.queryBus.execute(query)
-    return recipes.map(recipe => RecipeResponse.fromDomain(recipe))
+    return this.queryBus.execute(query)
   }
 
   @Put(':id')
