60b5bf7a037aa2527523281c480c3773a2fcb368
Pedro J <pjjimenezochoa@gmail.com>
2026-09-02 20:55:19 -0500

    fix(establishment): extend specific DomainException subclasses instead of generic DomainException

--- src/contexts/establishment/establishment/domain/exceptions/establishment-not-configured.exception.ts
+++ src/contexts/establishment/establishment/domain/exceptions/establishment-not-configured.exception.ts
@@ -1,6 +1,6 @@
-import { DomainException } from '@shared/domain/exceptions/domain.exception'
+import { NotFoundException } from '@shared/domain/exceptions/domain.exception'
 
-export class EstablishmentNotConfigured extends DomainException {
+export class EstablishmentNotConfigured extends NotFoundException {
   constructor() {
     super(
       'Establishment is not configured. Complete the setup wizard via POST /establishment/settings.'
